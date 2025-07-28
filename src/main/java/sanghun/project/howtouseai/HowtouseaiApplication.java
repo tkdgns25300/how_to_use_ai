27e72fa620bf02331cf430eec4f9215deb094cb4
이상훈 <tkdgns25300@gmail.com>
2025-07-28 22:05:03 +0900

    Initial Setup

---
+++ src/main/java/sanghun/project/howtouseai/HowtouseaiApplication.java
@@ -0,0 +1,13 @@
+package sanghun.project.howtouseai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtouseaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtouseaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sanghun/project/howtouseai/HowtouseaiApplicationTests.java
@@ -0,0 +1,13 @@
+package sanghun.project.howtouseai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtouseaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
